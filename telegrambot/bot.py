f90b0782108606d55518f08ba77d132e023d1a7e
zanastas <83847982+zanastas@users.noreply.github.com>
2024-10-04 18:49:19 +0800

    import

---
+++ telegrambot/bot.py
@@ -0,0 +1,9 @@
+import io
+import os 
+import logging
+import pathlib
+from typing import Any
+import dotenv
+import base64
+import qrcode
+from telegram import Update, InlineKeyboardButton, InlineKeyboardMarkup

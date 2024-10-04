84e0e74bc6ed58d0c858f3eac473ac14c24d431f
zanastas <83847982+zanastas@users.noreply.github.com>
2024-10-04 20:29:31 +0800

    add base model and pydantic

--- telegrambot/bot.py
+++ telegrambot/bot.py
@@ -22,3 +22,36 @@ from typing import List, Optional, Type, TypeVar
 from pydantic import BaseModel, Field, StrictStr
 import requests
 from datetime import date
+
+T = TypeVar('T', bound=BaseModel) 
+SYSTEM_PROMPT = pathlib.Path("system_prompt.txt").read_text()
+USDC_CONTRACT_ADDRESS = '0x2C9678042D52B97D27f2bD2947F7111d93F3dD0D'
+CHALLENGE_CONTRACT_ADDRESS = '0x833589fCD6eDb6E08f4c7C32D4f71b54bdA02913'
+CHAIN_ID = "534351" # scroll sepolia chain id
+
+
+dotenv.load_dotenv()
+openai.api_key = os.getenv('OPENAI_API_KEY')
+
+logging.basicConfig(
+    format='%(asctime)s - %(name)s - %(levelname)s - %(message)s',
+    level=logging.INFO
+)
+
+def load_json_as_model(path: str, model: Type[T]) -> T:
+    with open(path, 'r') as file:
+        return model.model_validate_json(file.read())
+
+def store_json_as_model(path: str, model: BaseModel):
+    with open(path, 'w') as file:
+        file.write(model.model_dump_json())
+
+class StoreableBaseModel(BaseModel):
+    def save(self, path: str):
+        store_json_as_model(path, self)
+    
+    @classmethod
+    def load(cls, path: str):
+        return load_json_as_model(path, cls)  
+    
+    

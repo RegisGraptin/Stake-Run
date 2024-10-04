3df1abfc610e5ee0fc7510f8b299f4402e1b52dd
rere <regis-graptin@sfr.fr>
2024-10-04 16:41:49 +0800

    feat: join current challenge

--- contract/src/StakeAndRun.sol
+++ contract/src/StakeAndRun.sol
@@ -7,7 +7,7 @@ contract StakeAndRun {
     address owner;
 
     // Keep track of the challenge
-    uint256 challengeId; 
+    uint256 counterChallengeId; 
 
     // Defined if a challenge is currently running or not
     bool currentChallengeRunning; 
@@ -23,20 +23,26 @@ contract StakeAndRun {
         bool isCompleted;
     }
 
-    // Record all the challenges
-    mapping(uint256 => Challenge) internal challenges;
+    struct UserMetadata {
+        string telegramHandler;
+        uint256 kilometersRunned;
+    }
 
+    // Record all the challenges
+    mapping(uint256 => Challenge) challenges;
+
+    mapping(uint256 => mapping(address => bool)) participants;
+
+    mapping(uint256 => mapping(address => UserMetadata)) userInfo;
 
     /// Events 
     event NewChallenge(uint256 id, uint256 duration);
-
-
+    event NewUser(uint256 challengeId, address member);
 
     constructor() {
         owner = msg.sender;
         currentChallengeRunning = false;
     }
-
 
     function createNewChallenge(
         string memory name,
@@ -48,7 +54,6 @@ contract StakeAndRun {
         require(startTime > block.timestamp, "Start time need to be in the future.");
         require(startTime < endTime, "Start time need to be before end time.");
 
-
         Challenge memory challenge = Challenge({
             name: name,
             stakingAmount: stakingAmount,
@@ -59,11 +64,40 @@ contract StakeAndRun {
             isCompleted: false
         });
 
-        challenges[challengeId] = challenge;
+        challenges[counterChallengeId] = challenge;
 
-        emit NewChallenge(challengeId, durationDays);
+        emit NewChallenge(counterChallengeId, durationDays);
         
-        challengeId++;
+        counterChallengeId++;
+    }
+
+    function getChallengeDetail(uint256 challengeId) public view returns (Challenge memory) {
+        return challenges[challengeId];
+    }
+
+    function isParticipant(uint256 challengeId, address user) public view returns (bool) {
+        return participants[challengeId][user];
+    }
+
+
+    function joinChallenge(
+        uint256 challengeId,
+        string memory telegramHandler
+    ) payable external {
+        // Check the challenge has not started yet
+        require(challengeId == counterChallengeId - 1, "Challenge does not exist");
+        require(challenges[challengeId].startTime > block.timestamp, "Challenge has already start!");
+        require(msg.value >= challenges[challengeId].stakingAmount, "Not enough eth");
+        require(!participants[challengeId][msg.sender], "Already a participant");
+
+        participants[challengeId][msg.sender] = true;
+        userInfo[challengeId][msg.sender] = UserMetadata({
+            telegramHandler: telegramHandler,
+            kilometersRunned: 0
+        });
+
+        // Emit event
+        emit NewUser(challengeId, msg.sender);
     }
 
 

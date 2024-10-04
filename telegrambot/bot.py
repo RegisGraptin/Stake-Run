22470b069ece72779f47e5c538fa4eac4f961d0b
zanastas <83847982+zanastas@users.noreply.github.com>
2024-10-04 18:51:49 +0800

    import

--- telegrambot/bot.py
+++ telegrambot/bot.py
@@ -6,4 +6,19 @@ from typing import Any
 import dotenv
 import base64
 import qrcode
+import telegram
 from telegram import Update, InlineKeyboardButton, InlineKeyboardMarkup
+from telegram.constants import ChatAction
+from telegram.ext import filters, MessageHandler, ApplicationBuilder, CommandHandler, ContextTypes, CallbackQueryHandler
+import uuid
+import requests
+import json
+from datetime import datetime, time, timedelta
+#import pytz
+from apscheduler.schedulers.asyncio import AsyncIOScheduler
+import random
+import openai
+from typing import List, Optional, Type, TypeVar
+from pydantic import BaseModel, Field, StrictStr
+import requests
+from datetime import date

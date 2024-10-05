9f7e0cb7e2bd484969200e490d661b0360921160
rere <regis-graptin@sfr.fr>
2024-10-05 22:48:37 +0800

    feat: remove contact link

--- front/src/components/Header.tsx
+++ front/src/components/Header.tsx
@@ -32,9 +32,6 @@ export const Header = () => {
                             <li>
                                 <a href="/dashboard/create" className="block py-2 px-3 text-gray-900 rounded hover:bg-gray-100 md:hover:bg-transparent md:hover:text-blue-700 md:p-0">Create</a>
                             </li>
-                            <li>
-                                <a href="#" className="block py-2 px-3 text-gray-900 rounded hover:bg-gray-100 md:hover:bg-transparent md:hover:text-blue-700 md:p-0">Contact</a>
-                            </li>
                         </ul>
                     </div>
                 </div>
